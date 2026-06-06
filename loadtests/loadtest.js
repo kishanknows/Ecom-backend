5e32047d4d92900cc0d07d2b51d38155a999659f
Kishan <kishanknows@gmail.com>
2026-06-06 19:16:15 +0530

    add load test

---
+++ loadtests/loadtest.js
@@ -0,0 +1,21 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "30s", target: 100 },
+    { duration: "30s", target: 200 },
+    { duration: "30s", target: 300 },
+    { duration: "30s", target: 400 },
+    { duration: "30s", target: 500 },
+    { duration: "30s", target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get("https://ecom.kishanknows.in/products");
+
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+  });
+}
